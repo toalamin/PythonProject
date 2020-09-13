e3a5c863b990f67f21b6b893772f2b8103dd1aa7
toalamin <toalaminbd@gmail.com>
2020-09-13 17:47:27 +0600

    contact us data save done!

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contactus
 
 # Create your views here.
 
 
 def contact(request):
+    if request.method == 'POST':
+       name = request.POST.get('name')
+       email = request.POST.get('email')
+       subject = request.POST.get('subject')
+       message = request.POST.get('message')
+       contactData = Contactus(name=name,email=email,subject=subject,message=message)
+       contactData.save()
     return render(request,'front/contact.html')
